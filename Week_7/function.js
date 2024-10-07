fea17853c3709aa483d9a73170b46c5872834adf
Ala Williams <alicja.m.williams@gmail.com>
2024-10-07 12:24:33 +0300

    new functions

--- Week_7/function.js
+++ Week_7/function.js
@@ -2,3 +2,10 @@ function printHelloMessage() {
   console.log('Hello!');
 }
 printHelloMessage();
+
+function calculate(number1, number2) {
+  let sum = number1 + number2;
+  process.stdout.write(sum.toString());
+}
+
+calculate(1, 2);
