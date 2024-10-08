2ed8d9aff2c9a20f1c3ddd861ddf8fa3199605a3
Ala Williams <alicja.m.williams@gmail.com>
2024-10-08 15:10:09 +0300

    task5

---
+++ Week_7/task4.js
@@ -0,0 +1,14 @@
+// 4 Task 4 Circumference of a Circle
+// Create a program that calculates the circumference of a circle when radius is given (circumference = 2 * π *
+// radius). Call this method three times with different parameters and print out the results
+
+function calculateCircumference(radius) {
+  let result = 2 * Math.PI * radius
+  process.stdout.write('Circumference of a circle with radius ' + radius + ' is ' + result + '.\n')
+}
+
+calculateCircumference(5)
+
+calculateCircumference(10)
+
+calculateCircumference(20)
---
+++ Week_7/task5.js
@@ -0,0 +1,33 @@
+// Create a program that has the following functionality:
+// 1. Ask the user for a string and store it in a variable.
+// 2. replaceSmileys(text): Finds smileys in text and replaces them with corresponding text as
+// shown in the table below.
+// 3. replaceSmileysWithReplacer(text, replacer): Finds smileys in text and replaces them
+// with replacer.
+
+
+function replaceSmileys(string) {
+  string = string.replaceAll(':)', '*smiling*');
+  string = string.replaceAll(':(', '*sad*');
+  string = string.replaceAll(';)', '*winkwink*');
+  string = string.replaceAll('<3', '*love*');
+  return string;
+}
+
+function replaceSmileysWithReplacer(string, replacer) {
+  string = string.replaceAll(':)', replacer);
+  string = string.replaceAll(':(', replacer);
+  string = string.replaceAll(';)', replacer);
+  string = string.replaceAll('<3', replacer);
+  return string;
+}
+process.stdout.write('Please enter a string: ')
+process.stdin.on('data', function(string) {
+  let input = string.toString().trim();
+  let newString = replaceSmileys(input);
+   process.stdout.write('Replaced text: ' + newString + '\n')
+
+   newString = replaceSmileysWithReplacer(input, '*');
+   process.stdout.write('Replaced text (*):  ' + newString)
+  process.exit();
+});
