7ae2f20ce04ed3e3e3c6f1da0e5bde8ce815f18f
Ala Williams <alicja.m.williams@gmail.com>
2024-10-09 13:47:37 +0300

    fixing the structure

--- Week_7/mathQuiz.js
+++ Week_7/mathQuiz.js
@@ -2,32 +2,46 @@ function createCalculation(number1, number2) {
   return number1 + number2; 
 };
 
-let number1 = Math.floor(Math.random() * 10);
-let number2 = Math.floor(Math.random() * 10)
+
+let score = 0;
+let counter = 0;
 
 process.stdout.write('MATH QUIZ'.padStart(25, '*').padEnd(40, '*') + '\n\n');
 
+function createQuestion() {
+  let number1 = Math.floor(Math.random() * 10);
+  let number2 = Math.floor(Math.random() * 10);
 
-let counter = 0;
-while (counter < 10) {
+  process.stdout.write(`${number1} + ${number2} = `)
 
-  process.stdout.write(number1 + ' + ' + number2 + ' = ')
-  process.stdin.on('data', function(inputFromUser){
-    let number = Number(inputFromUser);   
-    // process.stdout.write(number.toString()) 
-    
-      if (createCalculation(number1, number2) === number) {
-        counter =+ 1
-        process.stdout.write(`\nCORRECT!\nYour score is: ${counter}`);
-        
-      } else (
-        process.stdout.write('INCORRECT\n')
-      )
   
-    process.exit();
-    }
-    )} 
+  process.stdin.once('data', function(inputFromUser){
+    let number = Number(inputFromUser);   
 
+      if (createCalculation(number1, number2) === number) {
+        score += 1
+        process.stdout.write(`\nCORRECT!\nYour score is: ${score} \n\n`);
+      
+      } else {
+        process.stdout.write('INCORRECT\n\n')
+    }
+
+      counter += 1;
+
+   
+    
+  
+    if (counter === 10) {
+      process.stdout.write(`Game is OVER. Your score is: ${score} \n`);
+      process.exit();
+
+    } else {
+      createQuestion()
+    }
+  
+  });}
+  
+  createQuestion()
  
 
 
