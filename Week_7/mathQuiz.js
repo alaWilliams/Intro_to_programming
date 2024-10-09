59e371edc5e1a50752aff21b599bc71a95d3c46e
Ala Williams <alicja.m.williams@gmail.com>
2024-10-09 14:05:09 +0300

    new commit

--- Week_7/mathQuiz.js
+++ Week_7/mathQuiz.js
@@ -5,10 +5,12 @@ function createCalculation(number1, number2) {
 
 let score = 0;
 let counter = 0;
+let wrongAnswer = 0;
 
 process.stdout.write('MATH QUIZ'.padStart(25, '*').padEnd(40, '*') + '\n\n');
 
 function createQuestion() {
+
   let number1 = Math.floor(Math.random() * 10);
   let number2 = Math.floor(Math.random() * 10);
 
@@ -23,13 +25,17 @@ function createQuestion() {
         process.stdout.write(`\nCORRECT!\nYour score is: ${score} \n\n`);
       
       } else {
+        wrongAnswer += 1;
         process.stdout.write('INCORRECT\n\n')
     }
 
       counter += 1;
 
    
-    
+    if (wrongAnswer === 5) {
+      process.stdout.write(`You answered incorrectly 5 times. The game is over. You score is: ${score} \n\n`);
+      process.exit();
+    }
   
     if (counter === 10) {
       process.stdout.write(`Game is OVER. Your score is: ${score} \n`);
