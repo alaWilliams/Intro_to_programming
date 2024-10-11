7c55fe0ee06bb1a6a80d2f50cae5285391635c04
Ala Williams <alicja.m.williams@gmail.com>
2024-10-11 09:05:58 +0300

    the rest of the game

--- Week_7/rockPaperScissors.js
+++ Week_7/rockPaperScissors.js
@@ -5,12 +5,19 @@ process.stdout.write('Rock, paper, scissors game. Choose one: ')
 process.stdin.on('data', function(input){
   let choice = input.toString().trim().toLowerCase();
   let computerChoice = choices[index]
-
   if (choice !== 'rock' && choice !=='paper' && choice !== 'scissors'){
-    process.stdout.write('Please choose rock, paper or scissors.')
+    process.stdout.write('Please choose rock, paper or scissors. \n')
   } else if (choice === computerChoice) {
       process.stdout.write(`It is a draw. You chose ${choice} and computer chose ${computerChoice}`)
-  }
+      process.exit();
+  } else if ((computerChoice === 'rock' && choice === 'scissors') || (computerChoice === 'scissors' && choice === 'paper') || (computerChoice === 'paper' && choice === 'rock')) {
+    process.stdout.write(`Computer won. You chose ${choice} and computer chose ${computerChoice}`)
+    process.exit();
+  } else {
+    process.stdout.write(`You won. You chose ${choice} and computer chose ${computerChoice}`)
+    process.exit();
+  };
+    
 
-  process.exit();
+  
 })
